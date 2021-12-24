177c6f30e3b56408538c9cf349e4f0784363fffe
MrK0t <vvvmr.kott@gmail.com>
2021-12-24 20:55:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BattleSystem
+ 
---
+++ Source/BattleSytem/Public/BSSphereComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "BSSphereComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class BATTLESYTEM_API UBSSphereComponent : public USphereComponent
+{
+	GENERATED_BODY()
+	
+};
